7325dccc3dbc76bb8ee1c38fc62adb985875cb30
unknown <arbi.nmaki919@gmail.com>
2021-03-05 09:32:18 +0700

    Commit fade-reveal

--- src/parts/Testimony.js
+++ src/parts/Testimony.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Fade from 'react-reveal/Fade'
 import TestimonyAccent from "assets/images/testimonial-landingpages-frame.jpg";
 
 import Star from "elements/Star";
@@ -7,6 +7,7 @@ import Button from "elements/Button";
 
 export default function Testimony({ data }) {
   return (
+    <Fade bottom>
     <section>
       <div className="row align-items-center">
         <div className="col-auto" style={{ marginRight: 70 }}>
@@ -51,5 +52,6 @@ export default function Testimony({ data }) {
         </div>
       </div>
     </section>
+    </Fade>
   );
 }
